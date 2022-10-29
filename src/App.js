53cecdcdab2e6eddbd89d338f045dc6c2d2f5e35
Taylan Özdemir <taylanozdemir@Taylan-MacBook-Pro.local>
2022-10-29 13:36:43 +0300

    first react codes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import logo from "./logo.svg";
+
 function App() {
-	return <div className="App">test2</div>;
+	return (
+		<div className="App">
+			<img src="/logo192.png" />
+			<img src={logo} />
+		</div>
+	);
 }
 
 export default App;
